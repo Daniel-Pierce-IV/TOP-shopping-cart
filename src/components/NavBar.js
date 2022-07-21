eb50168188a2005375210be8c2c153d54bb6c81e
Daniel Pierce <daniel.pierce.iv@gmail.com>
2022-07-20 20:23:43 -0500

    Redesign Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,26 @@
 import { Link, NavLink } from "react-router-dom";
+import cartImg from "../img/cart.png";
 
 const NavBar = () => {
   const activeClasses = "underline";
   const applyActiveClasses = ({ isActive }) => isActive && activeClasses;
 
   return (
-    <nav className="flex justify-between p-8 bg-orange-400">
-      <h1 className="text-4xl">
-        <Link to="/">Title</Link>
+    <nav className="flex justify-between items-center p-8 bg-[#202425]">
+      <h1 className="text-2xl text-[#e8e8e8] font-['Givonic'] tracking-widest">
+        <Link to="/">
+          TECHNO<span className="font-bold">LIFE</span>
+        </Link>
       </h1>
 
       <ul className="flex items-center gap-8 text-lg">
         <li>
-          <NavLink to="products" className={applyActiveClasses}>
-            Products
-          </NavLink>
-        </li>
-
-        <li>
           <NavLink to="cart" className={applyActiveClasses}>
-            Cart
+            <img
+              src={cartImg}
+              alt="Cart Icon"
+              className="w-[40px] invert opacity-[0.9]"
+            />
           </NavLink>
         </li>
       </ul>
